da0491dda412caa5fa25c45739ee19dc2b6b0fa6
Warat <warat.sat@gmail.com>
2024-06-28 22:31:17 +0700

    Update assignment

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,26 @@
-class EmailNotification { }
-
-class SMSNotification { }
+class EmailNotification { 
+    constructor(NotificationId,createdTime,content,receiver){
+        this.NotificationId=NotificationId
+        this.createdTime=createdTime
+        this.content=content
+        this.receiver=receiver
+        }
+    send(){
+        console.log(`Notification has been sent to ${this.receiver}`)
+    }
+    }
+class SMSNotification {
+    constructor(NotificationId,createdTime,content,phoneNumber){
+        this.NotificationId=NotificationId
+        this.createdTime=createdTime
+        this.content=content
+        this.phoneNumber=phoneNumber
+        }
+    send(){
+        console.log(`Notification has been sent to ${this.phoneNumber}`)
+    }
+ }
+ const testEmail=new EmailNotification("0","28/06/2024","test","test@techup.com")
+ const testSMS=new SMSNotification("1","28/06/2024","test","0987654321")
+testEmail.send()
+testSMS.send()
